fdc0d4c6eaed85d7d7a095dd3012738fe7e0e6eb
garyfinco <garyfinco@gmail.com>
2022-11-19 10:44:18 -0800

    added collapsable dendogram

--- rajputana.js
+++ rajputana.js
@@ -12,12 +12,6 @@ var svgTimeline = d3.select("body").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var svgDenogram = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
 var context = svgTimeline.append("g")
     .attr("class", "context")
     .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
@@ -101,7 +95,7 @@ d3.csv("sampleCsv.csv", function (error, data) {
         .call(brush)
         .call(brush.move, xScale.range());
 
-        //[xScale(1400), xScale(1600)]
+    //[xScale(1400), xScale(1600)]
 
 });
 
@@ -109,48 +103,188 @@ d3.csv("sampleCsv.csv", function (error, data) {
 function brushed(d) {
     xScale.domain([xScale2.invert(d3.event.selection[0]), xScale2.invert(d3.event.selection[1])]);
     svgTimeline.selectAll(".xaxis").call(xAxis);
-    
+
     svgTimeline.selectAll(".timeRect")
-      .attr("x", function(d){ return xScale(parseDate(d.start));})
-      .attr("width", function (d) { return (xScale(parseDate(d.end)) - xScale(parseDate(d.start))); });
+        .attr("x", function (d) { return xScale(parseDate(d.start)); })
+        .attr("width", function (d) { return (xScale(parseDate(d.end)) - xScale(parseDate(d.start))); });
 }
 
-d3.json("sample.json", function (error, data) {
+
+
+var svgDenogram = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("sample.json", function (error, treeData) {
     if (error) return console.error(error);
 
-    var cluster = d3.cluster()
-        .size([height, width - 100]);  // 100 is the margin I will have on the right side
+    var i = 0,
+        duration = 750,
+        root;
 
-    // Give the data to this cluster layout:
-    var root = d3.hierarchy(data, function (d) {
-        return d.children;
-    });
-    cluster(root);
+    // declares a tree layout and assigns the size
+    var treemap = d3.tree().size([height, width]);
 
-    svgDenogram.selectAll('path')
-        .data(root.descendants().slice(1))
-        .enter()
-        .append('path')
-        .attr("d", function (d) {
-            return "M" + d.y + "," + d.x
-                + "C" + (d.parent.y + 50) + "," + d.x
-                + " " + (d.parent.y + 150) + "," + d.parent.x // 50 and 150 are coordinates of inflexion, play with it to change links shape
-                + " " + d.parent.y + "," + d.parent.x;
-        })
-        .style("fill", 'none')
-        .attr("stroke", '#ccc');
+    // Assigns parent, children, height, depth
+    root = d3.hierarchy(treeData, function (d) { return d.children; });
+    root.x0 = height / 2;
+    root.y0 = 0;
 
-    svgDenogram.selectAll("g")
-        .data(root.descendants())
-        .enter()
-        .append("g")
-        .attr("transform", function (d) {
-            return "translate(" + d.y + "," + d.x + ")"
-        })
-        .append("circle")
-        .attr("r", 7)
-        .style("fill", "#69b3a2")
-        .attr("stroke", "black")
-        .style("stroke-width", 2);
+    // Collapse after the second level
+    root.children.forEach(collapse);
+
+    update(root);
+
+    // Collapse the node and all it's children
+    function collapse(d) {
+        if (d.children) {
+            d._children = d.children
+            d._children.forEach(collapse)
+            d.children = null
+        }
+    }
+
+    function update(source) {
+
+        // Assigns the x and y position for the nodes
+        var treeData = treemap(root);
+
+        // Compute the new tree layout.
+        var nodes = treeData.descendants(),
+            links = treeData.descendants().slice(1);
+
+        // Normalize for fixed-depth.
+        nodes.forEach(function (d) { d.y = d.depth * 180 });
+
+        // ****************** Nodes section ***************************
+
+        // Update the nodes...
+        var node = svgDenogram.selectAll('g.node')
+            .data(nodes, function (d) { return d.id || (d.id = ++i); });
+
+        // Enter any new modes at the parent's previous position.
+        var nodeEnter = node.enter().append('g')
+            .attr('class', 'node')
+            .attr("transform", function (d) {
+                return "translate(" + source.y0 + "," + source.x0 + ")";
+            })
+            .on('click', click);
+
+        // Add Circle for the nodes
+        nodeEnter.append('circle')
+            .attr('class', 'node')
+            .attr('r', 1e-6)
+            .style("fill", function (d) {
+                return d._children ? "#FFE562" : "#fff";
+            });
+
+        // Add labels for the nodes
+        nodeEnter.append('text')
+            .attr("dy", ".35em")
+            .attr("x", function (d) {
+                return d.children || d._children ? -13 : 13;
+            })
+            .attr("text-anchor", function (d) {
+                return d.children || d._children ? "end" : "start";
+            })
+            .text(function (d) { return d.data.name; });
+
+        // UPDATE
+        var nodeUpdate = nodeEnter.merge(node);
+
+        // Transition to the proper position for the node
+        nodeUpdate.transition()
+            .duration(duration)
+            .attr("transform", function (d) {
+                return "translate(" + d.y + "," + d.x + ")";
+            });
+
+        // Update the node attributes and style
+        nodeUpdate.select('circle.node')
+            .attr('r', 10)
+            .style("fill", function (d) {
+                return d._children ? "#FFE562" : "#fff";
+            })
+            .attr('cursor', 'pointer');
+
+
+        // Remove any exiting nodes
+        var nodeExit = node.exit().transition()
+            .duration(duration)
+            .attr("transform", function (d) {
+                return "translate(" + source.y + "," + source.x + ")";
+            })
+            .remove();
+
+        // On exit reduce the node circles size to 0
+        nodeExit.select('circle')
+            .attr('r', 1e-6);
+
+        // On exit reduce the opacity of text labels
+        nodeExit.select('text')
+            .style('fill-opacity', 1e-6);
+
+        // ****************** links section ***************************
+
+        // Update the links...
+        var link = svgDenogram.selectAll('path.link')
+            .data(links, function (d) { return d.id; });
+
+        // Enter any new links at the parent's previous position.
+        var linkEnter = link.enter().insert('path', "g")
+            .attr("class", "link")
+            .attr('d', function (d) {
+                var o = { x: source.x0, y: source.y0 }
+                return diagonal(o, o)
+            });
+
+        // UPDATE
+        var linkUpdate = linkEnter.merge(link);
+
+        // Transition back to the parent element position
+        linkUpdate.transition()
+            .duration(duration)
+            .attr('d', function (d) { return diagonal(d, d.parent) });
+
+        // Remove any exiting links
+        var linkExit = link.exit().transition()
+            .duration(duration)
+            .attr('d', function (d) {
+                var o = { x: source.x, y: source.y }
+                return diagonal(o, o)
+            })
+            .remove();
+
+        // Store the old positions for transition.
+        nodes.forEach(function (d) {
+            d.x0 = d.x;
+            d.y0 = d.y;
+        });
+
+        // Creates a curved (diagonal) path from parent to the child nodes
+        function diagonal(s, d) {
+
+            path = `M ${s.y} ${s.x}
+            C ${(s.y + d.y) / 2} ${s.x},
+              ${(s.y + d.y) / 2} ${d.x},
+              ${d.y} ${d.x}`
+
+            return path
+        }
+
+        // Toggle children on click.
+        function click(d) {
+            if (d.children) {
+                d._children = d.children;
+                d.children = null;
+            } else {
+                d.children = d._children;
+                d._children = null;
+            }
+            update(d);
+        }
+    }
 
 });
